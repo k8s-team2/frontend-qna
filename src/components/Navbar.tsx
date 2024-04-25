f2b0b0dd79ddbf162386beb5c8d5c86808e9c752
unknown <kkudos@naver.com>
2024-04-25 22:53:56 +0900

    the first push by crystal 4.25

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import {
   NavbarContent,
   NavbarItem,
   Link,
+  Button
 } from "@nextui-org/react";
 import NicknameButton from "./NicknameButton";
 
@@ -13,10 +14,15 @@ const Navigation = () => {
   return (
     <>
       <Navbar position="static">
-        <NavbarBrand>
+        <NavbarBrand className="flex flex-row gap-3">
           <Link href="/">
             <p className="font-bold text-inherit">Cloud School Q&A</p>
           </Link>
+          <Button variant="bordered" color="primary">
+            <Link href="/attend/">
+            출석부
+            </Link>
+          </Button>
         </NavbarBrand>
 
         <NavbarContent justify="end">
